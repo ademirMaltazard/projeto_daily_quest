a70a19025997e25434aa5c2470e64749620c9ef8
ademirMaltazard <ademir.mk98@gmail.com>
2024-04-12 23:48:05 -0300

    alteração no arquivo main com conexao ao banco e intereção com as 3 telas. criação da tela de alerta. criação do primeiro estado de login.

--- main.py
+++ main.py
@@ -14,7 +14,48 @@ cursor = conexao.cursor(dictionary=True)
 cursor.execute('SELECT * FROM users')
 resultado = cursor.fetchall()
 print(resultado)
+loggeduser = {}
+
+#  FUNÇÕES
+def CheckLogin():
+    userLogin = loginScreen.lineEdit_login.text()
+    userPassword = loginScreen.lineEdit_password.text()
+
+    query = 'SELECT * FROM users WHERE login_user = %s'
+    cursor.execute(query, (userLogin,))
+    result = cursor.fetchall()
+    print(result)
+
+    if len(result) <= 0:
+        alertScreen.show()
+    else:
+        if result[0]['password_user'] == userPassword:
+            print(result[0]['login_user'])
+            global loggeduser
+            loggeduser = result[0]
+            loginScreen.close()
+            missionScreen.show()
+        else:
+            alertScreen.show()
+
+
+def CloseScreenAlert():
+    alertScreen.close()
+    loginScreen.lineEdit_login.setText('')
+    loginScreen.lineEdit_password.setText('')
+
+#  GERANDO UMA APLICAÇÃO
+app = QtWidgets.QApplication([])
+
+#  CARREGAR ARQUIVO UI
+loginScreen = uic.loadUi('loginScreen.ui')
+missionScreen = uic.loadUi('missionScreen.ui')
+alertScreen = uic.loadUi('alert.ui')
 
 #  AÇÕES
+loginScreen.pushButton_login.clicked.connect(CheckLogin)
+alertScreen.pushButton_confirm.clicked.connect(CloseScreenAlert)
 
-login_screen = uic.loadUi('loginScreen')
+#  EXIBIR NA TELA
+loginScreen.show()
+app.exec()
