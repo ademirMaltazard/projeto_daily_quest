166544a8365da4159f2eb46998976ba8a51f9c34
ademirMaltazard <ademir.mk98@gmail.com>
2024-05-11 13:16:05 -0300

    melhoria na checagem de usuario para login e limpeza dos campos de inserção, adição de condiçoes para criação de usuario, criação da função retornar tela

--- main.py
+++ main.py
@@ -6,24 +6,26 @@ loggedUser = {}
 
 #  FUNÇÕES
 def CheckLogin():
+    global loggedUser
     userLogin = loginScreen.lineEdit_login.text()
     userPassword = loginScreen.lineEdit_password.text()
 
     result = databaseCRUD().SearchOne(userLogin)
 
     if result is None:
-        print(result)
         alertScreen.show()
+        loginScreen.lineEdit_login.setText("")
+        loginScreen.lineEdit_password.setText("")
     else:
         if result['password_user'] == userPassword:
-            global loggedUser
             loggedUser = result
             ShowDailyQuest()
             loginScreen.close()
         else:
             result = None
             alertScreen.show()
-            print(result)
+            loginScreen.lineEdit_login.setText("")
+            loginScreen.lineEdit_password.setText("")
 
 def CreateNewUser():
     name = singinScreen.lineEdit_name.text()
@@ -33,7 +35,7 @@ def CreateNewUser():
     cod = singinScreen.lineEdit_code.text()
 
     print(name, login, password, confirmPassword, cod)
-    if password == confirmPassword:
+    if (password == confirmPassword and name != '' and login != ''):
         databaseCRUD().CreateNewUser(name, login, password, cod)
         singinScreen.lineEdit_name.setText('')
         singinScreen.lineEdit_login.setText('')
@@ -50,37 +52,40 @@ def CloseScreenAlert():
 def ShowDailyQuest():
     missionScreen.show()
     activeQuest = databaseCRUD().SearchExercise()
-    print(activeQuest)
     missionScreen.tableWidget_quest.setRowCount(len(activeQuest))
 
     row = 0
     for indice in activeQuest:
-        print((indice))
         missionScreen.tableWidget_quest.setItem(row, 0, QtWidgets.QTableWidgetItem(indice["quantity_exercise"]))
         missionScreen.tableWidget_quest.setItem(row, 1, QtWidgets.QTableWidgetItem(indice["name_exercise"]))
         row += 1
 
     activePunition = databaseCRUD().SerachPunition()
-    print(activePunition)
     missionScreen.tableWidget_punition.setRowCount(len(activePunition))
 
     row = 0
     for indice in activePunition:
-        print((indice))
         missionScreen.tableWidget_punition.setItem(row, 0, QtWidgets.QTableWidgetItem(indice["name_punition"]))
         missionScreen.tableWidget_punition.setItem(row, 1, QtWidgets.QTableWidgetItem(indice["quantity_punition"]))
         missionScreen.tableWidget_punition.setItem(row, 2, QtWidgets.QTableWidgetItem(indice["description_punition"]))
         row += 1
 
-def lodinToSingin():
-    loginScreen.close()
-    singinScreen.show()
-
-def returnScreen(ui1, ui2):
+def ReturnScreen(ui1, ui2):
     ui1.close()
     ui2.show()
-def back():
-    returnScreen(singinScreen, loginScreen)
+
+def SiginToLogin():
+    ReturnScreen(singinScreen, loginScreen)
+
+def LoginToSingin():
+    ReturnScreen(loginScreen, singinScreen)
+
+def MissionToLogin():
+    global loggedUser
+    ReturnScreen(missionScreen, loginScreen)
+    loginScreen.lineEdit_login.setText("")
+    loginScreen.lineEdit_password.setText("")
+    loggedUser = {}
 
 
 #  GERANDO UMA APLICAÇÃO
@@ -93,11 +98,12 @@ singinScreen = uic.loadUi('singinScreen.ui')
 alertScreen = uic.loadUi('alert.ui')
 
 #  AÇÕES
-loginScreen.pushButton_login.clicked.connect(CheckLogin)
-loginScreen.pushButton_singin.clicked.connect(lodinToSingin)
-singinScreen.pushButton_singin.clicked.connect(CreateNewUser)
 alertScreen.pushButton_confirm.clicked.connect(CloseScreenAlert)
-singinScreen.pushButton_back.clicked.connect(back)
+loginScreen.pushButton_login.clicked.connect(CheckLogin)
+loginScreen.pushButton_singin.clicked.connect(LoginToSingin)
+singinScreen.pushButton_singin.clicked.connect(CreateNewUser)
+singinScreen.pushButton_logout.clicked.connect(SiginToLogin)
+missionScreen.pushButton_logout.clicked.connect(MissionToLogin)
 
 #  EXIBIR NA TELA
 loginScreen.show()
